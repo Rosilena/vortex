387ced319739b0896d80e6b60a9cdfea0077fed2
mew27 <terzolopaolo@libero.it>
2026-05-21 21:55:52 +0200

    Fixed missing parenthesis

--- tests/regression/crypto/kernel.cpp
+++ tests/regression/crypto/kernel.cpp
@@ -98,7 +98,8 @@ int main() {
                   return vx_spawn_threads(1, &arg->grid_dim, &arg->block_dim, (vx_kernel_func_cb) kernel_decipher_128, arg);
             } else if (arg->aes_size == AES192) {
                   return vx_spawn_threads(1, &arg->grid_dim, &arg->block_dim, (vx_kernel_func_cb) kernel_decipher_192, arg);
-            
-            //return vx_spawn_threads(1, &arg->grid_dim, &arg->block_dim, (vx_kernel_func_cb) kernel_decipher_128, arg);
+            }
       }
+
+      return 0;
 }
